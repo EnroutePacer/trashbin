bd6c78840a025e1c7582db9bcfedd3c6a4fa4ee7
EnroutePacer <enternityknight@outlook.com>
2026-03-25 22:51:05 +0800

    add some comment

--- C/test.cpp
+++ C/test.cpp
@@ -1,22 +1,41 @@
+// 类的基本用法，const 修饰类内方法
+
+// 单例的创建（static 用法）
+
+// 
+
+
 #include <iostream>
 #include <vector>
 #include <string>
 
+// 类，const
 class person
 {
 private:
-    int age;
+    mutable int age; // mutable 使变量能在 const 限制下仍改变
     std::string gender;
 
 public:
     person(int a,std::string b): age(a),gender(b) {}
-    int getage(){
+    
+    // 前面的 const 限制使用者利用 ref 特性修改返回值，从而影响私有变量
+
+    // 后面的 const 规定方法内变量不能被修改，
+    // 并将返回值的 this 指针变为 const 类型，使得 main 函数里的 const 对象可以调用方法
+
+    const int& getage() const{
         return age;
     }
-    std::string getgender(){
+    const std::string& getgender() const{
         return gender;
     }
-
+    
+    const int& growup(int& year) const
+    {
+        age += year;
+        return age;
+    }
 };
 
 // 单例
@@ -30,6 +49,7 @@ public:
         static singleton TheOne;
         return TheOne;
     }
+
     void Claim()
     {
         std::cout << "put data" << std::endl;
@@ -52,17 +72,20 @@ public:
     }
 };
 
-int* singleton::level=new int;
+int* singleton::level=new int; // 静态变量必须在类外初始化
+
 
 int main(void)
 {
-    person Mike(13,"Male");
-    std::cout << Mike.getage() << Mike.getgender() << std::endl;
+    const person Mike(13,"Male"); // const 对象在调用方法时，只能用 const 修饰过的方法
+    int year_pass = 5;
+    std::cout << Mike.growup(year_pass) <<" "<< Mike.getgender() << std::endl;
 
     singleton::GetOne().Claim();
     singleton::GetOne().SetLevel(1);
     singleton::GetOne().AddLevel();
     singleton::GetOne().Log();
 
+    std::cin.get();
     return 0;
 }
